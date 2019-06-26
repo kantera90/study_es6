1b28c1238feb230510c30553cf934789788596cd
h_oyagi <h_oyagi@dialonet.co.jp>
2019-06-26 16:45:37 +0900

    Helloクラスを外部から参照するまで完了

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require("path");
+
+module.exports = {
+  mode: 'development',
+  devServer: {
+    open: true,
+    openPage: "index.html",
+    contentBase: path.join(__dirname, "public"),
+    watchContentBase: true,
+    port: 8080,
+  },
+  output: {
+    libraryTarget: 'umd'
+  }
+}
+
